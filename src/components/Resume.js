089269515f42c150670e8dbfe0acef672693ca67
diyavaland <151962108+diyavaland@users.noreply.github.com>
2024-12-07 23:45:38 -0700

    Updated Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -29,7 +29,7 @@ function Resume() {
         <p>Class Representative in Level 4, Computer Programming</p>
       </section>
       <section>
-        <a href="/Profile_Diya.pdf" download="Profile_Diya.pdf" className="download-button">
+        <a href="/Diya Valand.pdf" download="Profile_Diya.pdf" className="download-button">
           Download Resume
         </a>
       </section>
